e17846248d33930388fdbb1052353469fa851e3f
Deepti gaharwar <deepti.gaharwar@gmail.com>
2024-02-16 16:06:07 +0000

    update

--- EKS-Validation-Vars/variables.tf
+++ EKS-Validation-Vars/variables.tf
@@ -7,7 +7,7 @@ variable "desired_capacity" {
   description = "Desired number of worker nodes"
   type        = number
   validation {
-    condition     = var.desired_capacity > 0 && var.desired_capacity <= 3
+    condition     = var.desired_capacity != 0 && var.desired_capacity <= 3
     error_message = "Desired capacity must be between 1 and 10"
   }
 }
